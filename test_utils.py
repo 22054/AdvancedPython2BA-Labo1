7465cc2f2fcb8bf12e6041b60c782ab01211b001
Deuvaert Victor <22054@ecam.be>
2024-02-20 10:07:17 +0100

    Write test_roots

--- test_utils.py
+++ test_utils.py
@@ -5,9 +5,9 @@ def test_fact():
     assert utils.fact(5) == 120
 
 def test_roots():
-    assert utils.roots(4, -10, 6) == (1)
-    assert utils.roots(2, 0, -2) == (-1, 1)
-    assert utils.roots(2, 3, 3) == ()
+    assert utils.roots(1, 2, 1) == tuple([-1])
+    assert utils.roots(2, 0, -2) == tuple([-1, 1])
+    assert utils.roots(2, 3, 3) == tuple()
 
 def test_integrate():
     assert utils.integrate('x^2 - 2x', 1, 4) == 6

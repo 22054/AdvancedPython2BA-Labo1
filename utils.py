e310455e5665c4a76d0fed90cf0520e6dd6b5c5d
Deuvaert Victor <22054@ecam.be>
2024-02-20 10:07:51 +0100

    Write roots()

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from scipy import integrate as integr
+from math import sqrt
 
 def fact(n):
 	"""Computes the factorial of a natural number.
@@ -21,7 +22,20 @@ def roots(a, b, c):
 	Post: Returns a tuple with zero, one or two elements corresponding
 		to the roots of the ax^2 + bx + c polynomial.
 	"""
-	pass
+	delta = b**2 - 4*a*c
+	if delta < 0:
+		return tuple()
+	if delta == 0:
+		rep = -b/(2*a)
+		return tuple([rep])
+	if delta > 0:
+		rep1 = (-b+sqrt(delta))/(2*a)
+		rep2 = (-b-sqrt(delta))/(2*a)
+		if rep1 > rep2:
+			temp = rep1
+			rep1 = rep2
+			rep2 = temp
+		return tuple([rep1, rep2])
 
 def integrate(function, lower, upper):
 	"""Approximates the integral of a fonction between two bounds

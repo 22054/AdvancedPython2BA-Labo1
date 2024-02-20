3134995a684fa3c7edc6c2e401fa8d1484e93b33
Deuvaert Victor <22054@ecam.be>
2024-02-20 09:27:08 +0100

    Write test_roots()

--- test_utils.py
+++ test_utils.py
@@ -3,11 +3,11 @@ import utils
 
 def test_fact():
     assert utils.fact(5) == 120
-    pass
 
 def test_roots():
-    # À compléter...
-    pass
+    assert utils.roots(4, -10, 6) == (1)
+    assert utils.roots(2, 0, -2) == (-1, 1)
+    assert utils.roots(2, 3, 3) == ()
 
 def test_integrate():
     # À compléter...
